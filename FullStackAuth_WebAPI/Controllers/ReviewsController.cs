b2806c8cbb2da82d6404b24c7559fd60f568c791
mike focken <mikefocken@gmail.com>
2023-12-20 09:39:46 -0600

    compted review controller in VS. git commit -m'completed review controller in vs

--- FullStackAuth_WebAPI/Controllers/ReviewsController.cs
+++ FullStackAuth_WebAPI/Controllers/ReviewsController.cs
@@ -1,7 +1,10 @@
-﻿using FullStackAuth_WebAPI.DataTransferObjects;
+﻿using FullStackAuth_WebAPI.Data;
+using FullStackAuth_WebAPI.DataTransferObjects;
 using FullStackAuth_WebAPI.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -13,50 +16,58 @@ namespace FullStackAuth_WebAPI.Controllers
     [ApiController]
     public class ReviewsController : ControllerBase
     {
-        // Get api/<ReviewsController>
-        [HttpGet]
-        public IActionResult GetReviews()
+        private readonly ApplicationDbContext _context;
+
+        public ReviewsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //Post: Api/Reviews
+        [HttpPost, Authorize]
+        public IActionResult Post([FromBody]Review data)
         {
             try
+            {     // Retrieve the authenticated user's ID from the JWT token  
+                string userId = User.FindFirstValue("id");
+
+                // If the user ID is null or empty, the user is not authenticated, so return a 401 unauthorized response
+                if (string.IsNullOrEmpty(userId))
+                {
+                    return Unauthorized();
+
+                }
+
+                // Set the car's owner ID  the authenticated user's ID we found earlier             
+                data.UserId=userId;
+
+                // Add the car to the database and save changes
+                _context.Reviews.Add(data); ;
+
+                if (!ModelState.IsValid)
+                {   // If the car model state is invalid, return a 400 bad request response with the model state errors
+                    return BadRequest(ModelState);
+                }
+                _context.SaveChanges();
+
+                // Return the newly created car as a 201 created response
+                return StatusCode(201, data);
+
+
+            }
+            
+            catch (Exception ex)
             {
-                var cars = _context.Cars.Select(c => new CarWithUserDto
-                { 
-                    Id = c.Id,
-                    Make = c.Make,
-                    Model = c.Model,
-                    Year = c.Year,
-                    Owner = new UserForDisplayDto
-                    {
-                        Id = c.Owner.Id,
-                        FirstName = c.Owner.FirstName,
-                        LastName = c.Owner.LastName,
-                        UserName = c.Owner.UserName,
-                    }
-                }).ToList();
-
+                return StatusCode(500, ex.Message);
             }
-
-
-
-            }
-
-
-
-
-
-
-
-
+            
 
         }
 
-        // POST api/<ReviewsController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
 
 
     }
+
+        
 }
 
